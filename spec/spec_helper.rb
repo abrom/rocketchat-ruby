bea0610f0600be523e0314d78b64e633520eff82
Andrew Bromwich <a.bromwich@gmail.com>
2018-03-08 16:02:44 +1000

    Fixed up latest code style issues

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'simplecov'
 SimpleCov.start
 
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'rocketchat'
 
 require 'json'
