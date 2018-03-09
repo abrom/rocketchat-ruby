83ce8d01d35cd25f828261ff0f00b7e730f86715
CloverHearts <cloverheartsdev@gmail.com>
2018-03-09 18:42:29 +0900

    implemented channels.list.joined

--- lib/rocket_chat/messages/channel.rb
+++ lib/rocket_chat/messages/channel.rb
@@ -40,6 +40,25 @@ module RocketChat
         response['channels'].map { |hash| RocketChat::Room.new hash } if response['success']
       end
 
+      #
+      # channels.list.joined REST API
+      # @param [Integer] offset Query offset
+      # @param [Integer] count Query count/limit
+      # @param [Hash] sort Query field sort hash. eg `{ msgs: 1, name: -1 }`
+      # @param [Hash] fields Query fields to return. eg `{ name: 1, ro: 0 }`
+      # @param [Hash] query The query. `{ active: true, type: { '$in': ['name', 'general'] } }`
+      # @return [Room[]]
+      # @raise [HTTPError, StatusError]
+      #
+      def list_joined(offset: nil, count: nil, sort: nil, fields: nil, query: nil)
+        response = session.request_json(
+            '/api/v1/channels.list.joined',
+            body: build_list_body(offset, count, sort, fields, query)
+        )
+
+        response['channels'].map { |hash| RocketChat::Room.new hash } if response['success']
+      end
+
       # Keys for set_attr:
       # * [String] description A room's description
       # * [String] join_code Code to join a channel
