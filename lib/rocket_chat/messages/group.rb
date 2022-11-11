f818507e3cc1913fe100c679b4745cb4d8adf75e
Boris Peterbarg <boris@seekingalpha.com>
2022-11-11 02:16:42 +0200

    Add ability to set_attr channel/group custom_fields (#40)
    * Add ability to set_attr channel/group custom_fields, announcement and default
    * Support RC API endpoint groups.create param extraData
    
    The extraData param allows setting arbitrary attributes on the RC Room.
    Added to the API in RC v3.12.0 by Rocket.Chat PR 20617, commit 2ad2d0bd7ed
    
    Co-authored-by: Noach Magedman <noach@seekingalpha.com>

--- lib/rocket_chat/messages/group.rb
+++ lib/rocket_chat/messages/group.rb
@@ -92,13 +92,15 @@ module RocketChat
       end
 
       # Keys for set_attr:
+      # * [String] announcement Announcement for the channel
+      # * [Hash] custom_fields Custom fields for the channel
       # * [String] description A room's description
       # * [String] purpose Alias for description
       # * [Boolean] read_only Read-only status
       # * [String] topic A room's topic
       # * [Strong] type c (channel) or p (private group)
       def self.settable_attributes
-        %i[description purpose read_only topic type]
+        %i[announcement custom_fields description purpose read_only topic type]
       end
     end
   end
